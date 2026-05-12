2a9fd9e885f2bef6b6910af6b7e5dcff2824c0d1
Laura Costa <laurinhacosta174@gmail.com>
2026-05-12 19:40:51 -0300

    Alteração

---
+++ Multiplicador.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Multiplicador {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite o primeiro número: ");
+        int a = entrada.nextInt();
+		System.out.print("Digite o segundo número: ");
+        int b = entrada.nextInt();
+        System.out.println("Resultado: " + (a * b));
+    }
+}
